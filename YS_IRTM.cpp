f6ac05e717ed888e29bd23dee8e22e115bcc19e2
WuJi <710977702@qq.com>
2021-01-24 23:52:46 +0800

    Revert "Revert "YS-IRTM红外线编码模块""
    This reverts commit 49fde8e27aefd5dc33940af799b64c961bf44e32.

---
+++ YS_IRTM.cpp
@@ -0,0 +1,36 @@
+// 
+// 
+// 
+
+#include "YS_IRTM.h"
+
+
+
+
+YS_IRTMClass::YS_IRTMClass(int RX, int TX)
+{
+	RXD = RX;
+	TXD = TX;
+	mySerial = &SoftwareSerial::SoftwareSerial(8, 9);
+	mySerial->begin(9600);
+}
+
+size_t YS_IRTMClass::write(uint8_t byte[], int len)
+{
+	mySerial->write(0XA1);
+	mySerial->write(0XF1);
+	for (int i = 0; i < len; i++) {
+		mySerial->write(byte[i]);
+	}
+	return size_t(len);
+}
+
+int YS_IRTMClass::available()
+{
+	return mySerial->available();
+}
+
+int YS_IRTMClass::read()
+{
+	return mySerial->read();
+}
